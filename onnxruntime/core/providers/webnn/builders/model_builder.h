51ade3ba49e7ea463560183f040f17228c900e7c
zesongw <zesong.wang@intel.com>
2023-03-22 09:27:31 +0800

    Fix nit

--- onnxruntime/core/providers/webnn/builders/model_builder.h
+++ onnxruntime/core/providers/webnn/builders/model_builder.h
@@ -33,7 +33,7 @@ class ModelBuilder {
   void AddOperand(const std::string& name, const emscripten::val& operand);
   // Use the buffers to persist WebNN allocated data like transposed weight, which ensures the validity during inference session.
   std::vector<std::unique_ptr<uint8_t[]>> mem_persist_buffers_;
-  // Add a operand (allocate persist buffer and move the ownership to mem_persist_buffers_)
+  // Add a constant operand (allocate persist buffer and move the ownership to mem_persist_buffers_)
   Status AddOperandFromPersistMemoryBuffer(
       const std::string& name, const void* buffer,
       const size_t size, const std::vector<uint32_t> shape, const size_t element_size = 4);
